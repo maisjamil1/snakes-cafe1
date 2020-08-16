cffe0117310eec67948d27ccd3035d8205d6513d
maisjamil1 <maisjamil17118@gmail.com>
2020-08-16 15:46:23 +0300

    1

---
+++ snakes-cafe/snakes_cafe/snakes_cafe.py
@@ -0,0 +1,63 @@
+print('**************************************')
+print('**    Welcome to the Snakes Cafe!   **')
+print('**    Please see our menu below.    **')
+print('**')
+print('** To quit at any time, type "quit" **')
+print('*'*38)
+print('**    Welcome to the Snakes Cafe!   **')
+print('**    Please see our menu below.    **')
+print('**')
+print('** To quit at any time, type "quit" **')
+print('*'*38)
+menu="""
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+"""
+print(menu)
+
+print('*'*38)
+print('What would you like to order?')
+print('*'*38)
+# while input()!='quit':
+#   print(input())
+foodli=['wings','cookies','spring Rolls','salmon','steak','meat tornado','a Literal garden','ice cream','cake','pie','coffee','tea','unicorn tears']
+odersli=[]
+while True:
+    order=input()
+    if order.lower()=='quit':
+        break
+    elif order.lower() in foodli:
+        odersli.append(order.lower())
+        # print(odersli)
+        orderCount=odersli.count(order.lower())
+        print(f"** {orderCount} order of {order} have been added to your meal **")
+    else:
+        print('sorry,this order is not exist')
+
+
+
+
+
+
---
+++ snakes-cafe/tests/test_snakes_cafe.py
@@ -0,0 +1,4 @@
+from snakes_cafe import __version__
+def test_version():
+    assert __version__ == '0.1.0'
+
